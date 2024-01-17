79fe3e9de76762b17ce918146c8a557c22949de2
BrainStackOverFlow <48990146+BrainStackOverFlow@users.noreply.github.com>
2024-01-17 13:42:54 +0200

    Use TAP testing framework

--- test/test_example.c
+++ test/test_example.c
@@ -6,7 +6,13 @@ void test_sanity(void)
     TEST_CHECK(example_add(1,2) == 1 + 2);
 }
 
+void test_sanity2(void)
+{
+    TEST_CHECK(example_add(1,-1) == 2);
+}
+
 TEST_LIST = {
     { "sanity", test_sanity },
+    { "sanity2", test_sanity2 },
     { NULL, NULL }
 };
